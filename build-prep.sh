6674add36a79e4de021f9264e1da42629bb415b0
Jared Ottley <jared.ottley@alfresco.com>
2017-11-17 15:14:21 -0700

    DEPLOY-206 Move Hello World UI Deployment to New GitHub Repo
    Fix environment variable names to be specific to this project

--- build-prep.sh
+++ build-prep.sh
@@ -4,7 +4,7 @@ set -o errexit
 . ./build.properties
 
 # mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:copy \
-#    -Dartifact=$SERVICE_MAVEN_GROUP_ID:$SERVICE_MAVEN_ARTIFACT_ID:$SERVICE_MAVEN_VERSION \
+#    -Dartifact=$UI_MAVEN_GROUP_ID:$UI_MAVEN_ARTIFACT_ID:$UI_MAVEN_VERSION \
 #    -DoutputDirectory=./
 
-unzip $SERVICE_MAVEN_ARTIFACT_ID-$SERVICE_MAVEN_VERSION.zip
+unzip $UI_MAVEN_ARTIFACT_ID-$UI_MAVEN_VERSION.zip
