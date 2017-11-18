870fde08dce1d04a17fccc5e4ad50dbb324de91b
Jared Ottley <jared.ottley@alfresco.com>
2017-11-17 21:02:34 -0700

    DEPLOY-206 Move Hello World UI Deployment to New GitHub Repo
    Uncomment maven copy command

--- build-prep.sh
+++ build-prep.sh
@@ -3,8 +3,8 @@ set -o errexit
 
 . ./build.properties
 
-# mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:copy \
-#    -Dartifact=$UI_MAVEN_GROUP_ID:$UI_MAVEN_ARTIFACT_ID:$UI_MAVEN_VERSION \
-#    -DoutputDirectory=./
+ mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:copy \
+    -Dartifact=$UI_MAVEN_GROUP_ID:$UI_MAVEN_ARTIFACT_ID:$UI_MAVEN_VERSION \
+    -DoutputDirectory=./
 
 unzip $UI_MAVEN_ARTIFACT_ID-$UI_MAVEN_VERSION.zip
