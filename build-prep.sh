e891a06f0e9c215fb15352be898548a6185a94af
Jared Ottley <jared.ottley@alfresco.com>
2017-11-20 15:08:26 -0700

    DEPLOY-206 Move Hello World UI Deployment to New GitHub Repo
    Fix build-prep.sh script
    -Error in finding file

--- build-prep.sh
+++ build-prep.sh
@@ -4,11 +4,10 @@ set -o errexit
 . ./build.properties
 
 ## Do we already have the tar?  It may be outdated...let's remove it.
-ls -1 $UI_MAVEN_ARTIFACT_ID-*.tar > /dev/null 2>&1
-if [ "$?" = "0" ]; then
+if [ -f $UI_MAVEN_ARTIFACT_ID-*.tar ]; then
     rm $UI_MAVEN_ARTIFACT_ID-*.tar
 fi
 
- mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:copy \
+ mvn org.apache.maven.plugins:maven-dependency-plugin:3.0.2:copy \
     -Dartifact=$UI_MAVEN_GROUP_ID:$UI_MAVEN_ARTIFACT_ID:$UI_MAVEN_VERSION:tar \
     -DoutputDirectory=./
