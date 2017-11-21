525b8644d1261bdd53a0b9acecc7c62c994140d5
Jared Ottley <jottley@gmail.com>
2017-11-20 19:54:17 -0700

    DEPLOY-206 Move Hello World UI Deployment to New GitHub Repo
    Move maven plugin back to version 2.8 because there is an issue with the newer versions when used on a bamboo agent.

--- build-prep.sh
+++ build-prep.sh
@@ -8,6 +8,6 @@ if [ -f $UI_MAVEN_ARTIFACT_ID-*.tar ]; then
     rm $UI_MAVEN_ARTIFACT_ID-*.tar
 fi
 
- mvn org.apache.maven.plugins:maven-dependency-plugin:3.0.2:copy \
+ mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:copy \
     -Dartifact=$UI_MAVEN_GROUP_ID:$UI_MAVEN_ARTIFACT_ID:$UI_MAVEN_VERSION:tar \
     -DoutputDirectory=./
